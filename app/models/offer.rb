cfbed065009bec3c15b2daa0c2a6d7a258e1f868
Renan Lucas <renan.lucas07@gmail.com>
2020-06-17 17:39:41 -0300

    add method to set state on offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -9,12 +9,26 @@ class Offer < ApplicationRecord
   validates :url, uri: true
   validates :description, length: { maximum: 500 }
 
-  before_save :set_state
+  before_create :set_state
 
   def to_s
     advertiser_name
   end
 
   private
+  def set_state
+    current_time = DateTime.now
 
+    if current_time >= self.starts_at
+      if self.ends_at.nil?
+        self.state = State::ENABLED
+      elsif current_time <= self.ends_at
+        self.state = State::ENABLED
+      else
+        self.state = State::DISABLED
+      end
+    else
+      self.state = State::DISABLED
+    end
+  end
 end
